6f3a2f1d678fd67978c71f87422b4fda1a9598a5
Jeff <sjeffshah@gmail.com>
2019-09-16 21:10:26 -0400

    calculator in progress

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,43 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.previousOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear () {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+
+} 
+ const numberButtons = document.querySelectorAll('[data-number]')
+ const operationButtons = document.querySelectorAll('[data-operation]')
+ const operationButtons = document.querySelector('[data-equals]')
+ const operationButtons = document.querySelector('[data-delete]')
+ const operationButtons = document.querySelector('[data-all-clear]')
+ const operationButtons = document.querySelector('[data-previous-operand]')
+ const operationButtons = document.querySelector('[data-current-operand]')
  
